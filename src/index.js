4753ee6df29a85cf30842068f83674bf93629805
emericgautier <emericgtr@gmail.com>
2022-09-19 12:32:13 +0200

    put data in the store

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
 
+// redux
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { getPosts } from "./actions/post.action";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getPosts());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
