aa6f98b7a242a83c3d84966ea293e81e4df68baf
emericgautier <emericgtr@gmail.com>
2022-09-21 11:35:18 +0200

    edit & delete post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Like from "./Like";
+import { isEmpty } from "./Utils";
 
 const Post = ({ post }) => {
+  const [editToggle, setEditToggle] = useState(false);
+  const [editContent, setEditContent] = useState(post.content);
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    const postData = {
+      title: post.title,
+      author: user[0].pseudo,
+      content: editContent,
+      likes: post.likes,
+      id: post.id,
+    };
+    dispatch(editPost(postData));
+    setEditToggle(false);
+  };
+
   return (
     <div className="post">
+      {!isEmpty(user[0]) && user[0].pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            onClick={() => setEditToggle(!editToggle)}
+            src="./icons/edit.svg"
+            alt="edit"
+          />
+          <img
+            onClick={() => setEditToggle(!editToggle)}
+            src="./icons/delete.svg"
+            alt="delete"
+          />
+        </div>
+      )}
       <h2>{post.title}</h2>
       <img
         className="post-img"
@@ -11,10 +46,20 @@ const Post = ({ post }) => {
         alt="img-post"
       />
 
-      <p>{post.content}</p>
-
+      {editToggle ? (
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea
+            defaultValue={post.content}
+            onChange={(e) => setEditContent(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Valider modification" />
+        </form>
+      ) : (
+        <p>{post.content}</p>
+      )}
       <div className="author">
         <h5>{post.author}</h5>
+        <Like post={post} />
       </div>
     </div>
   );
